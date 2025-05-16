677a6c156f169bd12c007bdb8cd6975ab726cb85
Nico Fuchs <nico@EyeShmac2.local>
2025-05-16 20:33:31 +0200

    switched client to template

--- client/vite/config.prod.mjs
+++ client/vite/config.prod.mjs
@@ -1,46 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 const phasermsg = () => {
     return {
         name: 'phasermsg',
         buildStart() {
-            process.stdout.write(`Building for production...\n`)
+            process.stdout.write(`Building for production...\n`);
         },
         buildEnd() {
-            const line =
-                '---------------------------------------------------------'
-            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`
-            process.stdout.write(`${line}\n${msg}\n${line}\n`)
-
-            process.stdout.write(`✨ Done ✨\n`)
-        },
+            const line = "---------------------------------------------------------";
+            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+            
+            process.stdout.write(`✨ Done ✨\n`);
+        }
     }
-}
+}   
 
 export default defineConfig({
     base: './',
-    logLevel: 'warning',
+    logLevel: 'warn',
     build: {
         rollupOptions: {
             output: {
                 manualChunks: {
-                    phaser: ['phaser'],
-                },
-            },
+                    phaser: ['phaser']
+                }
+            }
         },
         minify: 'terser',
         terserOptions: {
             compress: {
-                passes: 2,
+                passes: 2
             },
             mangle: true,
             format: {
-                comments: false,
-            },
-        },
+                comments: false
+            }
+        }
     },
     server: {
-        port: 8080,
+        port: 8080
     },
-    plugins: [phasermsg()],
-})
+    plugins: [
+        phasermsg()
+    ]
+});
