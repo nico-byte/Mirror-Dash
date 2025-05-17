c415bbbace30e6a9ea76b468e6d7d2326564957d
Matthias Laton <laton19578@hs-ansbach.de>
2025-05-17 11:17:40 +0200

    Update Components and map

--- client/src/game/components/FloatingPlatformScript.js
+++ client/src/game/components/FloatingPlatformScript.js
@@ -1,24 +1,19 @@
-export default class FloatingPlatformScript {
-	constructor(gameObject) {
-		this.gameObject = gameObject;
-		gameObject.scene.events.once("update", this.start, this);
-	}
+// FloatingPlatformScript.js
 
-	start() {
-		const obj = this.gameObject;
+class FloatingPlatformScript extends ScriptNode {
 
-		// Physik-Sicherheit (auch falls vom Editor nicht gesetzt)
-		obj.setImmovable(true);
-		if (obj.body) obj.body.allowGravity = false;
+    awake() {
+        const platform = this.gameObject;
 
-		// Tween: auf/ab bewegen
-		obj.scene.tweens.add({
-			targets: obj,
-			y: obj.y - 100,     // Höhe der Bewegung
-			duration: 2000,
-			yoyo: true,
-			repeat: -1,
-			ease: "Sine.inOut"
-		});
-	}
+        this.scene.tweens.add({
+            targets: platform,
+            y: platform.y - 80,
+            duration: 2000,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sine.easeInOut'
+        });
+    }
 }
+
+
